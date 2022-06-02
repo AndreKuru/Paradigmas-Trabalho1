93d9e987ba93fcabf78ca4723f5f8edf666b8f64
paolabel <pa0la.abel@hotmail.com>
2022-06-02 00:04:51 -0300

    - Parametrização parcial do módulo de matrizes - Início do módulo de matriz de marcações   - Marcar matriz ok   - Limpar linha e coluna ok   - Falta separar a matriz em caixas e verificar marcações por caixa

--- Main.hs
+++ Main.hs
@@ -1,18 +1,16 @@
 import Matrix
+import MarkingsMatrix
 
 myMatrix :: Matrix
 myMatrix = [[1,2,5], [2,3,2], [2,4,8]]
 
 main = do
-    printMatrix myMatrix
-    print(getMatrixRow 2 myMatrix)
-    print(getNRowsMatrix myMatrix)
-    print(getNColumnsMatrix myMatrix)
-    print(getMatrixColumn 2 myMatrix)
-    print(getMatrixElement 1 1 myMatrix)
-    print(splitArray 1 4 [1,2,3,4,5])
-    printMatrix(splitMatrixLines 1 2 myMatrix)
-    printMatrix(setMatrixRow 2 [0, 0, 0] myMatrix)
-    printMatrix(setMatrixElement 1 1 59 myMatrix)
-    printMatrix (setMatrixColumn 2 [0,0,0] myMatrix)
-    printMatrix(createEmptyMatrix 7 5)
+    let myMatrix2 = fillNewMatrix 7 5 False
+    printMarkingsMatrix myMatrix2
+    let myMatrix3 = markMatrix 2 2 myMatrix2
+    let myMatrix4 = markMatrix 2 4 myMatrix3
+    let myMatrix5 = markMatrix 0 2 myMatrix4
+    let myMatrix6 = markMatrix 3 4 myMatrix5
+    let myMatrix7 = markMatrix 0 1 myMatrix6
+    printMarkingsMatrix myMatrix7
+    printMarkingsMatrix(clearRowAndColumn 2 2 myMatrix7)
---
+++ MarkingsMatrix.hs
@@ -0,0 +1,62 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Use :" #-}
+{-# HLINT ignore "Evaluate" #-}
+
+module MarkingsMatrix where
+
+import Matrix
+
+type MarkingsArray = [Bool]
+type MarkingsMatrix = [MarkingsArray]
+
+-- insere ordem da matriz para descobrir tamanho das "caixas" de comparadores
+boxSize :: Int -> [Int]
+boxSize 0 = [0, 0]
+boxSize 1 = [1, 1]
+boxSize 4 = [2, 2]
+boxSize 6 = [3, 2]
+boxSize 9 = [3, 3]
+
+-- TODO
+-- faz com que cada linha seja equivalente a uma caixa
+-- assume que a matriz é quadrada
+-- boxes :: MarkingsArray -> MarkingsMatrix
+-- boxes (a:b) =
+--     let boxsize = boxSize getNColumnsMatrix (a:b)
+
+-- TODO
+-- divideRowPerBox :: Int -> Int -> MarkingsArray -> MarkingsMatrix
+-- divideRowPerBox 0 _ row = []
+-- divideRowPerBox 1 _ row = []
+-- divideRowPerBox boxWidth (a:b) boxesPerRow=
+--     if boxWidth >= getArrayLength row then
+--         [row]
+--     else
+--         [a] ++ divideRowPerBox boxWidth-1 b 
+
+markMatrix :: Int -> Int -> MarkingsMatrix -> MarkingsMatrix
+markMatrix row column = setMatrixElement row column True
+
+-- quando é oficialmente anotado o menor número de alguma caixa, é preciso 
+-- limpar as marcações da linha e coluna do elemento
+clearRowAndColumn :: Int -> Int -> MarkingsMatrix -> MarkingsMatrix
+clearRowAndColumn _ _ [] = []
+clearRowAndColumn row column matrix = do
+    let clearArray = fillNewArray (getNColumnsMatrix matrix) False
+    let partiallyClearMatrix = setMatrixRow row clearArray matrix
+    setMatrixColumn column clearArray partiallyClearMatrix
+
+printMarkingsMatrix :: MarkingsMatrix -> IO ()
+printMarkingsMatrix [] = putStrLn []
+printMarkingsMatrix (a:b) = do
+    putStrLn(markingsArrayString a)
+    printMarkingsMatrix b
+
+markingsMatrixString :: MarkingsMatrix -> String
+markingsMatrixString [] = []
+markingsMatrixString (a:b) = markingsArrayString a ++ markingsMatrixString b
+
+markingsArrayString :: MarkingsArray -> String
+markingsArrayString [] = []
+markingsArrayString (a:b) = show a ++ " " ++ markingsArrayString b
