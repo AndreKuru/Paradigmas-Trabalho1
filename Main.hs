b9a0589c47b543e0f17a5382e68335bd8284af4f
paolabel <pa0la.abel@hotmail.com>
2022-06-07 16:09:32 -0300

    comentar main

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import MarkingsMatrix
 import PuzzleSolver
 import Backtracker
 
+-- exemplo de matriz de operadores para tabuleiro 4x4
 operatorMatrix :: OperatorMatrix
 operatorMatrix = [['<', '|', '>', '|'],
                   ['^', '^', 'v', 'v'],
@@ -12,6 +13,8 @@ operatorMatrix = [['<', '|', '>', '|'],
                   ['v', 'v', '^', '^'],
                   ['>', '|', '<', '|']]
 
+-- exemplo 2 de matriz de operadores para tabuleiro 4x4
+operatorMatrix4 :: OperatorMatrix
 operatorMatrix4 = [['<', '|', '<', '|'], 
                    ['v', '^', '^', 'v'], 
                    ['<', '|', '>', '|'], 
@@ -20,6 +23,8 @@ operatorMatrix4 = [['<', '|', '<', '|'],
                    ['v', '^', '^', 'v'], 
                    ['>', '|', '>', '|']]
 
+-- exemplo de matriz de operadores para tabuleiro 6x6
+operatorMatrix6 :: OperatorMatrix
 operatorMatrix6 = [
   ['<', '|', '>', '|', '>', '|'],
   ['^', 'v', '^', '^', '^', 'v'],
@@ -33,6 +38,8 @@ operatorMatrix6 = [
   ['v', 'v', '^', '^', '^', 'v'],
   ['>', '|', '<', '|', '>', '|']]
 
+-- exemplo de matriz de operadores para tabuleiro 9x9
+operatorMatrix9 :: OperatorMatrix
 operatorMatrix9 = [
   ['<', '>', '|', '<', '<', '|', '>', '>', '|'],
   ['v', '^', '^', 'v', 'v', 'v', '^', 'v', '^'],
